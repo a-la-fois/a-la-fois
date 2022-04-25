d269884c8d238090359ea01beafd63fc194d8384
tzubairov <tzubairov@yandex-team.ru>
2022-04-25 16:47:51 +0300

    add empty projects for messageProxy and docHandler

---
+++ packages/docHandler/jest.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: path.resolve(__dirname, 'src'),
+  testRegex: '.test.ts$',
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
