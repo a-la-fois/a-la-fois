2978c1a2f8190d57070ea1031613408b6c58480f
jhoysbou <neeroll21@gmail.com>
2022-07-26 19:16:07 +0300

    some changes

--- packages/docHandler/src/actor/actor.module.ts
+++ packages/docHandler/src/actor/actor.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ActorService } from './actor.service';
 
 @Module({
-  providers: [],
+  providers: [ActorService],
+  exports: [ActorService],
 })
 export class ActorModule {}
