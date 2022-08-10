17cb9e88a2ec25bddcbcb85b52a802418d7e7d8f
jhoysbou <neeroll21@gmail.com>
2022-08-10 22:48:00 +0300

    docker compose run

--- packages/docHandler/src/main.ts
+++ packages/docHandler/src/main.ts
@@ -2,9 +2,9 @@ import { DaprServer } from '@dapr/dapr';
 import { DocHandler } from './docHandler';
 
 const daprHost = "127.0.0.1";
-const daprPort = "50000"; // Dapr Sidecar Port of this Example Server
-const serverHost = "127.0.0.1"; // App Host of this Example Server
-const serverPort = "50001"; // App Port of this Example Server
+const daprPort = "3500";
+const serverHost = "127.0.0.1";
+const serverPort = "3000";
 
 async function start() {
   const server = new DaprServer(serverHost, serverPort, daprHost, daprPort);
