5caf59c9222eed3dc552b49049c31dc71eea04db
jhoysbou <neeroll21@gmail.com>
2022-09-10 21:31:02 +0300

    useing username and password for kafka from env

--- packages/messageProxy/src/config/config.ts
+++ packages/messageProxy/src/config/config.ts
@@ -3,6 +3,8 @@ export default () => ({
         changesTopic: process.env.KAFKA_CHANGES_TOPIC || 'changes',
         hosts: process.env.KAFKA_HOSTS || '127.0.0.1:9092',
         caPath: process.env.KAFKA_CA_PATH || '',
+        username: process.env.KAFKA_USERNAME || '',
+        password: process.env.KAFKA_PASSWORD || '',
     },
     dapr: {
         host: process.env.DAPR_SIDECAR_HOST || '127.0.0.1',
