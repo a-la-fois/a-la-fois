c683213bc4522990ae73c6375e949d70c711894d
Daniil Gorokhov <kripa0cool@mail.ru>
2022-11-08 11:33:45 +0300

    chore(messageProxy): simplified jest.config

--- packages/messageProxy/jest.config.js
+++ packages/messageProxy/jest.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   preset: 'ts-jest',
-  testEnvironment: 'node',
   rootDir: path.resolve(__dirname, 'src'),
   testRegex: '.test.ts$',
   collectCoverageFrom: ['**/*.(t|j)s'],
