3d1ec053e6328fb46a4e0881335bd3b61252e011
Daniil Gorokhov <kripa0cool@mail.ru>
2022-11-08 11:32:38 +0300

    feat: added new microservies – api

---
+++ packages/api/jest.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  preset: 'ts-jest',
+  rootDir: path.resolve(__dirname, 'src'),
+  testRegex: '.test.ts$',
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+};
