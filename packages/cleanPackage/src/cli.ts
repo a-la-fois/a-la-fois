f028f4c6fb4b226ddd5b8024cb3f1a304a043f9e
Timur Zubairov <ifruty@gmail.com>
2022-11-09 16:08:10 +0100

    add clean package

---
+++ packages/cleanPackage/src/cli.ts
@@ -0,0 +1,17 @@
+import args from 'args';
+import { cleanPackage, restorePackage, getConfig } from './cleanPackage';
+console.log('A');
+args.option('clean', 'Clean package.json').option('restore', 'Restore package.json');
+
+const run = () => {
+    const flags = args.parse(process.argv);
+
+    if (flags.clean) {
+        const config = getConfig();
+        cleanPackage(config);
+    } else if (flags.restore) {
+        restorePackage();
+    }
+};
+
+run();
