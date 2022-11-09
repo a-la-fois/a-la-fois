a81a573040ce0d363069af2d5769fd849f70f728
Timur Zubairov <ifruty@gmail.com>
2022-11-09 16:38:34 +0100

    fix

--- packages/docHandler/src/library/index.ts
+++ packages/docHandler/src/library/index.ts
@@ -1,2 +1,2 @@
 export * from '../messages';
-export * from '../actor';
+export * from '../actor/docHandler.interface';
--- packages/messageProxy/cleanPackage.config.js
+++ packages/messageProxy/cleanPackage.config.js
@@ -1,3 +1,3 @@
 exports.default = {
-    keepDependencies: ['@a-la-fois/doc-handler', '@a-la-fois/nest-common'],
+    keepDependencies: [],
 };
