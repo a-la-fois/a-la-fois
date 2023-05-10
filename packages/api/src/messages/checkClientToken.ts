bd1974f0b78914c68dfec2ad19b5159df22bc9cc
jhoysbou <neeroll21@gmail.com>
2023-05-10 22:37:12 +0200

    changed token scheme

--- packages/api/src/messages/checkClientToken.ts
+++ packages/api/src/messages/checkClientToken.ts
@@ -8,10 +8,12 @@ export type CheckClientTokenResponse<TJWTPayload extends JWTPayload = JWTPayload
 
 export type JWTPayload = {
     consumerId: string;
+    userId: string;
+    expiredAt?: number; // unix time
     docs?: {
         id: string;
         rights: Right[];
     }[];
 };
 
-export type Right = 'read' | 'write';
+export type Right = 'read' | 'write' | 'noAccess';
