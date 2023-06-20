83f395ae30da719d542f243cced358147721e5c5
jhoysbou <neeroll21@gmail.com>
2023-06-20 10:55:53 +0200

    signConsumerToken restored

--- utils/signConsumerToken.js
+++ utils/signConsumerToken.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const uuid = require('uuid');
 
 const run = async () => {
-    const keyPath = path.resolve(__dirname, '../.dev/private_key_new');
+    const keyPath = path.resolve(__dirname, '../.dev/private_key');
 
     if (!fs.existsSync(keyPath)) {
         console.error(`No private key in: ${keyPath}\nRun "yarn setup:dev"`);
@@ -14,7 +14,7 @@ const run = async () => {
 
     const privateKey = fs.readFileSync(keyPath);
 
-    const token = jwt.sign({ consumerId: '6490d4dd10c307c2661f3efd' }, privateKey, {
+    const token = jwt.sign({ consumerId: '6433ed6c687b8fdaf97c4382' }, privateKey, {
         algorithm: 'RS256',
     });
 
