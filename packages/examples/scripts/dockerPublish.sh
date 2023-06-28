d0320194bd3a06b98032b938bd125fa63a90c51d
Timur <timur@8xr.io>
2023-06-28 11:35:19 +0100

    Add examples image build

---
+++ packages/examples/scripts/dockerPublish.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+DIR=$( dirname -- "$( readlink -f -- "$0"; )"; )
+ROOT_DIR="${DIR}/.."
+DOCKER_IMAGE="examples"
+VERSION=$( ${DIR}/../../../utils/getPackageVersion.js ${ROOT_DIR}/package.json; )
+TAG=ghcr.io/a-la-fois/${DOCKER_IMAGE}:${VERSION}
+
+docker build --no-cache --tag ${TAG} ${ROOT_DIR}
+docker push ${TAG}
