bc3dd4116734156480b779dca594627fcfb21121
Anastasia Kotova <annakarts24@gmail.com>
2023-12-06 16:19:03 +0100

    Fix version

--- packages/examples/src/main.tsx
+++ packages/examples/src/main.tsx
@@ -2,5 +2,4 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// @ts-ignore
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
