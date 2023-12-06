cfda501c2b31b721dbe8e970a3130abfa12ac646
Anastasia Kotova <annakarts24@gmail.com>
2023-12-06 15:36:45 +0100

    Fix publish script: downgrade react types version

--- packages/examples/src/main.tsx
+++ packages/examples/src/main.tsx
@@ -2,4 +2,5 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// @ts-ignore
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
